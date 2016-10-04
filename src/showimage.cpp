9015d7d350e6f818f525cab0b5ac85d9bc59aea3
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-10-04 12:12:28 +0200

    catkinized

--- src/showimage.cpp
+++ src/showimage.cpp
@@ -34,7 +34,9 @@ GLFWwindow* winit(int width,int height, const char * mymonitor)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	#ifdef GLFW_AUTO_ICONIFY
 	glfwWindowHint(GLFW_AUTO_ICONIFY, GL_FALSE);
+	#endif
 
 	int n;
 	
