430afc20159564834a09a81c228cc7089b52570e
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-10-04 12:56:01 +0200

    improved showimages as ros node

--- src/showimage.cpp
+++ src/showimage.cpp
@@ -4,6 +4,8 @@
 #include "glpp/imageproc.hpp"
 #include <Eigen/Geometry>
 #include <iostream>
+#include <ros/ros.h>
+
 
 using namespace glpp;
 #define COCO_ERR() std::cout
@@ -19,7 +21,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
-GLFWwindow* winit(int width,int height, const char * mymonitor)
+GLFWwindow* winit(int width,int height, std::string monitorname, bool fullscreen)
 {
 		
 	const char * title = "projector_screen";
@@ -40,29 +42,50 @@ GLFWwindow* winit(int width,int height, const char * mymonitor)
 
 	int n;
 	
-	GLFWmonitor * monitor = mymonitor != 0 ? 0 : glfwGetPrimaryMonitor();
-	auto monitors = glfwGetMonitors(&n);
-	for(int i = 0; i < n; i++)
+	GLFWmonitor * monitor = 0;
+
+	if(fullscreen)
 	{
-		if(strcmp(glfwGetMonitorName(monitors[i]),mymonitor) == 0)
+		if(monitorname.empty())
+			monitor =  glfwGetPrimaryMonitor();
+		else
 		{
-			monitor = monitors[i];
-			break;
+			auto monitors = glfwGetMonitors(&n);
+			for(int i = 0; i < n; i++)
+			{
+				if(strcmp(glfwGetMonitorName(monitors[i]),monitorname.c_str()) == 0)
+				{
+					monitor = monitors[i];
+					break;
+				}
+			}
+			if(!monitor)
+			{
+				std::cout << "cannot find monitor " << monitorname << "\n Listing monitors:\n";
+				for(int i = 0; i < n; i++)
+					std::cout << " " << i << " " << glfwGetMonitorName(monitors[i]) << std::endl;
+				monitor = glfwGetPrimaryMonitor();
+			}
 		}
 	}
-	if(!monitor)
+
+	GLFWwindow * window;
+
+	if(width == 0)
 	{
-		std::cout << "cannot find monitor\n";
-		for(int i = 0; i < n; i++)
-			std::cout << " " << i << " " << glfwGetMonitorName(monitors[i]) << std::endl;
-		return 0;
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+		glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+		glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+		glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+		glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);		
+		window  = glfwCreateWindow( mode->width, mode->height, title, monitor, NULL);
 	}
-	
-	GLFWwindow * window = glfwCreateWindow( width, height, title, monitor, NULL);
+	else
+		window  = glfwCreateWindow( width, height, title, monitor, NULL);
 
 	if(!window)
 	{
-		std::cout<<"Not window"<<std::endl;
+		std::cout<<"Cannot create window " << width << " " << height << " monitor " << monitor << std::endl;
 		return 0;
 	}
 
@@ -77,14 +100,34 @@ GLFWwindow* winit(int width,int height, const char * mymonitor)
 
 int main(int argc, char **argv)
 {
-	if(argc < 2)
-	{	
-		std::cout << "required: imagename [monitorname]" << std::endl;
+	ros::init(argc, argv, "ros_minimal_node");
+	ros::NodeHandle nh;
+	ros::NodeHandle pnh("~");
+
+	std::string monitor;
+	std::string image;
+	int width;
+	int height;
+	bool fullscreen;
+	
+	pnh.param<std::string>("image",image,"");
+	if(image.empty())
+	{
+		if(argc < 2)
+		{	
+			std::cout << "required: imagename [monitorname]" << std::endl;
+			return -1;
+		}	
+		image = argv[1];
+	}
+	pnh.param("width",width,1280);
+	pnh.param("height",height,800);
+	pnh.param("fullscreen",fullscreen,true);
+	pnh.param<std::string>("monitor",monitor,"HDMI-0");
+
+	auto window = winit(width,height,monitor,fullscreen);
+	if(!window)
 		return -1;
-	}	
-	int width = 1280;
-	int height = 800;
-	auto window = winit(width,height,"HDMI-0");
 	glERR("opengl:after init app");
 
 	GLImageProc img;
@@ -107,15 +150,16 @@ int main(int argc, char **argv)
 		if(swap)
 		{
 			// LT LB RT RB
-			std::array<float,8> full({-0.516531,
+			/*std::array<float,8> full({-0.516531,
 										0.133457,
 										0.726285,
 										0.155106,
 										0.740697,
 	                                    -0.821361,
                                       -0.511139
-                                       -0.847909 });		      
-			//std::array<float,8> full({-0.5,1.0,   -0.5,-1.0,  0.5,1.0,   0.5,-1.0 });
+                                       -0.847909 });	
+                                       */	      
+			std::array<float,8> full({-0.5,1.0,   -0.5,-1.0,  0.5,1.0,   0.5,-1.0 });
 	        img.setVerticesClipSpace(full);
 	        swap = false;
 		}
